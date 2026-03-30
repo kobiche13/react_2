c00440a1efcd4eb68c09f7bc70b7ac445b1ce8d2
Kobiche <koba156@yandex.ru>
2026-03-30 23:40:34 +0300

    finish homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import itemBlue from './img/item-blue.jpg';
+import itemBlack from './img/item-black.jpg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  const item = {
+  brand: 'Tiger of Sweden',
+  title: 'Leonard coat',
+  description: 'Minimalistic coat in cotton-blend',
+  descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+  price: 399,
+  currency: '£'
+  }
+
+  class ShopItemClass extends React.Component {
+      constructor(props) {
+      super(props);
+      }
+      render() {
+        return( 
+        <div className="main-content">
+          <h2>{this.props.item.brand}</h2>
+          <h1>{this.props.item.title}</h1>
+          <h3>{this.props.item.description}</h3>
+          <div className="description">
+            {this.props.item.descriptionFull}
+          </div>
+          <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+          <div className="divider"></div>
+          <div className="purchase-info">
+            <div className="price">{this.props.item.currency}{this.props.item.price}</div>
+            <button>Добавить в корзину</button>
+          </div>
+      </div>
+        );
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="background-element">
+      </div>
+      <div className="highlight-window">
+        <div className='highlight-overlay'></div>
+      </div>
+      <div className="window">
+        <ShopItemClass item={item} />
+      </div>
     </div>
-  );
+)
 }
 
 export default App;
